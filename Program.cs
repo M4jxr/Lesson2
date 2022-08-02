c34b41d26c35c1766d8e2e30ab1e3c4ee1c7a0e5
M4jxr <cergeirizhuk@gmail.com>
2022-08-02 19:16:17 +0200

    firstcommit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿
+//int x = 100 / 50;
+//int sum1 = 100 + 50;
+//int sum2 = sum1 + 250;
+//int sum3 = sum2 + sum1;
+//sum3++;
+//sum3 = sum3 + 1;
+//sum3--;
+//sum3 = sum3 - 1;
+//x = 5;
+//x += 5;
+//x = x + 5;
+//x -= 5;
+//x = x - 5;
+//x *= 5;
+//x = x * 5;
+//x /= 5;
+//x = x / 5;
+
+//if ((x == y)&&(x == y))
+//{
+
+//}
+//-6 * x ^ 3 + 5 * x ^ 2 - 10 * x + 15
+
+int num = -6;
+double xx = -6 * Math.Pow(num, 3) + 5 * Math.Pow(num, 2) - 10 * num + 15;
+Console.WriteLine(xx);
+
+int num2 = -1;
+
+Console.WriteLine(Math.Abs(num2) * Math.Sin(num2));
+
+Console.WriteLine(Math.PI *(num2)*2);
+
+Console.WriteLine(Math.Max(num ,num2 ));
+
+
+
+
+var currentDate = DateTime.Today;
+//var toNewYear = DateTime.UtcNow;
+var date1 = new DateTime(2022, 1, 1 );
+
+var date2 = new DateTime(2023, 1, 1);
+Console.WriteLine("To New Year left :" + (date2-currentDate));
+Console.WriteLine("From New Year days : " + (currentDate - date1));
+
+
+
+
